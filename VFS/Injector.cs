22e4348d13c64cf54784b96828b2ff41ae7d740f
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-03-07 02:18:22 +0200

    Adding EasyHook package

---
+++ VFS/Injector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using EasyHook;
+
+namespace CKAN.VFS
+{
+    public class FileMonInterface : MarshalByRefObject
+    {
+        public void IsInstalled(Int32 InClientPID)
+        {
+            Console.WriteLine("FileMon has been installed in target {0}.\r\n", InClientPID);
+        }
+
+        public void OnCreateFile(Int32 InClientPID, String[] InFileNames)
+        {
+            for (int i = 0; i < InFileNames.Length; i++)
+            {
+                Console.WriteLine(InFileNames[i]);
+            }
+        }
+
+        public void ReportException(Exception InInfo)
+        {
+            Console.WriteLine("The target process has reported an error:\r\n" + InInfo.ToString());
+        }
+
+        public void Ping()
+        {
+        }
+    }
+
+    class Injector
+    {
+        static String ChannelName = null;
+
+        static void Main(string[] args)
+        {
+            Int32 TargetPID = 0;
+
+            if ((args.Length != 1) || !Int32.TryParse(args[0], out TargetPID))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: FileMon %PID%");
+                Console.WriteLine();
+
+                return;
+            }
+
+            try
+            {
+                try
+                {
+                    Config.Register(
+                        "A FileMon like demo application.",
+                        "FileMon.exe",
+                        "FileMonInject.dll");
+                }
+                catch (ApplicationException)
+                {
+
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                }
+
+                RemoteHooking.IpcCreateServer<FileMonInterface>(ref ChannelName, WellKnownObjectMode.SingleCall);
+
+                RemoteHooking.Inject(
+                    TargetPID,
+                    "FileMonInject.dll",
+                    "FileMonInject.dll",
+                    ChannelName);
+
+                Console.ReadLine();
+            }
+            catch (Exception ExtInfo)
+            {
+                Console.WriteLine("There was an error while connecting to target:\r\n{0}", ExtInfo.ToString());
+            }
+        }
+    }
+}
